c179da21a03ea5edf922fca4039a427394108074
SenzTuru <114452380+SenzTuru@users.noreply.github.com>
2022-09-26 17:57:03 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head<>
+        <title> Belajar CRUD</title>
+    </head>
+<body>
+    <h1>SMK TELKOM BANJARBARU</h1>
+    <a href="list.php"> Data siswa</a><p>
+    <a href="list.php"> Tambah Data siswa</a>
+</body>
+</html>
